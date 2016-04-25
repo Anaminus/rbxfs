ab12528e3eae76872841de2dcfa4e4c152863678
Anaminus <Anaminus@users.noreply.github.com>
2016-04-25 12:02:20 -0500

    Implement syncInAnalyzeActions.

--- sync-in.go
+++ sync-in.go
@@ -62,7 +62,213 @@ func syncInReadDir(opt *Options, cache SourceCache, dir []string, rules []rulePa
 	return
 }
 
+type OrderedInAction struct {
+	Priority int
+	Action   InAction
+}
+
+type SortInActionsByDepth []OrderedInAction
+
+func (s SortInActionsByDepth) Len() int {
+	return len(s)
+}
+func (s SortInActionsByDepth) Less(i, j int) bool {
+	if s[i].Action.Depth == s[j].Action.Depth {
+		return s[i].Priority < s[j].Priority
+	}
+	return s[i].Action.Depth < s[j].Action.Depth
+}
+func (s SortInActionsByDepth) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+type SortInActionsByDir []InAction
+
+func (s SortInActionsByDir) Len() int {
+	return len(s)
+}
+func (s SortInActionsByDir) Less(i, j int) bool {
+	return filepath.Join(s[i].Dir...) < filepath.Join(s[j].Dir...)
+}
+func (s SortInActionsByDir) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+type SortInSelections []InSelection
+
+func (s SortInSelections) Len() int {
+	return len(s)
+}
+func (s SortInSelections) Less(i, j int) bool {
+	if s[i].File == s[j].File {
+		return !s[i].Ignore && s[j].Ignore
+	}
+	return s[i].File < s[j].File
+}
+func (s SortInSelections) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
 func syncInAnalyzeActions(actions []InAction) []InAction {
+	// Conflicting Action pass: Resolve multiple actions selecting the same
+	// item. Also separate actions into individual selections.
+	{
+		type childItem struct {
+			path  string
+			child int
+		}
+
+		children := map[childItem]OrderedInAction{}
+		properties := map[string]OrderedInAction{}
+		for i, action := range actions {
+			sub := filepath.Join(action.Dir...)
+			for _, selection := range action.Selection {
+				path := filepath.Join(sub, selection.File)
+				for _, child := range selection.Children {
+					// Objects may conflict only if they are from the same
+					// source.
+					//
+					// TODO: This will probably change when reference tracking
+					// is introduced.
+					item := childItem{path, child}
+					// Latter actions override former actions.
+					if a, ok := children[item]; ok && a.Action.Depth > action.Depth {
+						// Do not override if existing action is deeper.
+						continue
+					}
+					children[item] = OrderedInAction{
+						Priority: i,
+						Action: InAction{
+							Depth: action.Depth,
+							Dir:   action.Dir,
+							Selection: []InSelection{InSelection{
+								File:     selection.File,
+								Ignore:   selection.Ignore,
+								Children: []int{child},
+							}},
+						},
+					}
+				}
+				for _, prop := range selection.Properties {
+					if a, ok := properties[prop]; ok && a.Action.Depth > action.Depth {
+						continue
+					}
+					properties[prop] = OrderedInAction{
+						Priority: i,
+						Action: InAction{
+							Depth: action.Depth,
+							Dir:   action.Dir,
+							Selection: []InSelection{InSelection{
+								File:       selection.File,
+								Ignore:     selection.Ignore,
+								Properties: []string{prop},
+							}},
+						},
+					}
+				}
+				for prop, value := range selection.Values {
+					// Properties conflict independently of the sources they
+					// are retrieved from.
+					if a, ok := properties[prop]; ok && a.Action.Depth > action.Depth {
+						continue
+					}
+					properties[prop] = OrderedInAction{
+						Priority: i,
+						Action: InAction{
+							Depth: action.Depth,
+							Dir:   action.Dir,
+							Selection: []InSelection{InSelection{
+								File:   selection.File,
+								Ignore: selection.Ignore,
+								Values: map[string]int{prop: value},
+							}},
+						},
+					}
+				}
+			}
+		}
+
+		sorted := make([]OrderedInAction, 0, len(children)+len(properties))
+		for _, action := range children {
+			sorted = append(sorted, action)
+		}
+		for _, action := range properties {
+			sorted = append(sorted, action)
+		}
+		sort.Sort(SortInActionsByDepth(sorted))
+
+		actions = make([]InAction, len(sorted))
+		for i, action := range sorted {
+			actions[i] = action.Action
+		}
+	}
+
+	// Merge pass 1: Combine selections of actions that apply to the same
+	// directory.
+	{
+		combine := map[string]*InAction{}
+		for i := 0; i < len(actions); i++ {
+			action := actions[i]
+			dir := filepath.Join(action.Dir...)
+			if c, ok := combine[dir]; ok {
+				c.Selection = append(c.Selection, action.Selection...)
+			} else {
+				combine[dir] = &action
+			}
+		}
+
+		out := make([]InAction, 0, len(actions))
+		for _, action := range combine {
+			out = append(out, *action)
+		}
+		actions = out
+	}
+
+	// Merge pass 2: Combine selections within each action that apply to the
+	// same source.
+	{
+		type selItem struct {
+			file   string
+			ignore bool
+		}
+
+		for i, action := range actions {
+			combine := map[selItem]*InSelection{}
+			for i := 0; i < len(action.Selection); i++ {
+				selection := action.Selection[i]
+				item := selItem{selection.File, selection.Ignore}
+				if c, ok := combine[item]; ok {
+					c.Children = append(c.Children, selection.Children...)
+					c.Properties = append(c.Properties, selection.Properties...)
+					for key, value := range selection.Values {
+						c.Values[key] = value
+					}
+				} else {
+					combine[item] = &selection
+				}
+			}
+
+			action.Selection = make([]InSelection, 0, len(combine))
+			for _, sel := range combine {
+				action.Selection = append(action.Selection, *sel)
+			}
+			actions[i] = action
+		}
+	}
+
+	// Sort pass: Sort actions, selections, and items.
+	{
+		for i, action := range actions {
+			for i, sel := range action.Selection {
+				sort.Ints(sel.Children)
+				sort.Strings(sel.Properties)
+				action.Selection[i] = sel
+			}
+			sort.Sort(SortInSelections(action.Selection))
+			actions[i] = action
+		}
+		sort.Sort(SortInActionsByDir(actions))
+	}
 	return actions
 }
 
